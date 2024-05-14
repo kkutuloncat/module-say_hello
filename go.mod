735f44db89f472603f417ff54b87c136e3a5feef
IANOONE <86506939+IANOONE@users.noreply.github.com>
2024-05-14 09:30:05 +0700

    update say hello with param name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/kkutuloncat/module-say_hello
+module github.com/kkutuloncat/module-say_hello/v2
 
 go 1.21.6
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package modulesayhello
 
-func SayHello() string {
-	return "Hello World"
+func SayHello(name string) string {
+	return "Hello " + name
 }
